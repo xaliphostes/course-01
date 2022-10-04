3c77a2610e6dd2ca6e592a3a96044404ec8dd110
Xaliphostes <fmaerten@gmail.com>
2022-10-04 18:44:10 +0200

    doc

--- src/lib/derivatives/Base.ts
+++ src/lib/derivatives/Base.ts
@@ -2,9 +2,9 @@
 // Base class for mathematical expressions
 //
 export interface Base {
-    eval(v: number): number // eval f at v
-    name(v: string): string // symbolic expr of f 
-    derive(): Base          // expression using Base derivative classes for f'
-    isNull(): boolean
-    isCst(): boolean
+    eval(x: number): number // numerical value of f at x
+    name(x: string): string // symbolic expression of f at x
+    derive(): Base          // symbolic expression of f'
+    isNull(): boolean       // this = 0 ?
+    isCst(): boolean        // this = constant ?
 }
